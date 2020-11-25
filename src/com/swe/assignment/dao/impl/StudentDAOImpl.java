f84e51df7b2bb32ddc2f957fc9094c44dc50ec97
Krishna94-code <64301138+Krishna94-code@users.noreply.github.com>
2020-11-25 18:27:10 -0500

    Update StudentDAOImpl.java

--- src/com/swe/assignment/dao/impl/StudentDAOImpl.java
+++ src/com/swe/assignment/dao/impl/StudentDAOImpl.java
@@ -1,16 +1,21 @@
 package com.swe.assignment.dao.impl;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
 
+import org.apache.kafka.common.serialization.Serdes;
+
 import com.swe.assignment.bean.StudentBean;
 import com.swe.assignment.dao.StudentDAO;
 
+
+import io.kcache.*;
 /**
  * Singleton Class to hold the database connection creation logic
  * 
@@ -24,34 +29,40 @@ public class StudentDAOImpl {
 	private EntityManager entityManager;
 	// variable to hold the entityManager factory object
 	private EntityManagerFactory factory;
+	private String bootstrapServers = "localhost:9092";
+	
+	private Cache<String, StudentBean[]> cache;
+	//StudentBean[] d =new StudentBean[1];
 
 	// setting the entityManager and connection during the object creation
-	private StudentDAOImpl() {
-		setEntityManager();
+	public StudentDAOImpl() {
+		//setEntityManager();
+		Properties props = new Properties();
+		props.put(KafkaCacheConfig.KAFKACACHE_BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		props.put(KafkaCacheConfig.KAFKACACHE_TOPIC_NUM_PARTITIONS_CONFIG, 3);
+		props.put(KafkaCacheConfig.KAFKACACHE_TOPIC_REPLICATION_FACTOR_CONFIG, 1);
+		props.put(KafkaCacheConfig.KAFKACACHE_TOPIC_CONFIG, "qe");
+		props.put(KafkaCacheConfig.KAFKACACHE_TOPIC_REQUIRE_COMPACT_CONFIG, true);
+
+		cache = new KafkaCache<>(new KafkaCacheConfig(props), Serdes.String(), Serdes.serdeFrom(new UserSerializer(), new UserDeserializer()));
+		cache.init();
+		//d[0]=new StudentBean("123","cde","cf","hj", "jk", 2,
+			//	"45", "jkkk", "fgt",
+				//"hjkk");
+		//cache.put("qe",d);
 	}
 
-	private void setEntityManager() {
-		// read the connection details from the persistence.xml file and create the
-		// factory
-		this.factory = Persistence.createEntityManagerFactory("StudentDB");
-		// create the entity manager object
-		this.entityManager = factory.createEntityManager();
-	}
-
-	/**
-	 * creating a singleton instance of database connection class
-	 * 
-	 * @return
-	 */
-	public static synchronized StudentDAOImpl getInstance() {
-		// if singleton instance is not available create an instance object
-		if (null == instance) {
-			instance = new StudentDAOImpl();
-		}
-		// else return the existing instance object
-		return instance;
-	}
-
+	
+			
+			public static synchronized StudentDAOImpl getInstance() {
+				// if singleton instance is not available create an instance object
+				if (null == instance) {
+					instance = new StudentDAOImpl();
+				}
+				// else return the existing instance object
+				return instance;
+			}
+	
 	/**
 	 * method to close the database connection
 	 */
@@ -67,25 +78,15 @@ public class StudentDAOImpl {
 	 * @throws Exception if the student cannot be saved
 	 */
 	public void saveToDatabase(StudentBean studentBean) throws Exception {
-		try {
-			// begin a transaction
-			entityManager.getTransaction().begin();
-			// save the studentDAO object
-			entityManager.persist(new StudentDAO(studentBean));
-			// commit the transaction
-			entityManager.getTransaction().commit();
-		} catch (Exception ex) {
-			// if any exception is thrown like already student existing rethrow the
-			// exception after printing
-			System.out.println(ex.getMessage());
-			try {
-				entityManager.getTransaction().rollback();
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-			}
+		System.out.println("insidesave");
+		System.out.println(cache.get("qe"));
 
-			throw ex;
-		}
+		StudentBean[] surveys = cache.get("qe");
+		
+		StudentBean[] nsurveys = new StudentBean[surveys.length+1];
+		System.arraycopy(surveys, 0, nsurveys, 0, surveys.length);
+		nsurveys[surveys.length] = studentBean;
+		cache.put("qe", nsurveys);
 
 	}
 
@@ -97,23 +98,32 @@ public class StudentDAOImpl {
 	 */
 	public List<Object> readStudentIds() throws Exception {
 		// create a query to select all the student ids
-		Query qry = entityManager.createQuery("select std.id from StudentDAO std");
-		List<Object> studIDList = qry.getResultList();
-		return studIDList;
+		System.out.println("read");
+		//Query qry = entityManager.createQuery("select std.id from StudentDAO std");
+		//List<Object> studIDList = qry.getResultList();
+		//return studIDList;
+		StudentBean[] surveys = cache.get("qe");
+		List<Object> s1= new ArrayList<Object>();
+		//disconnect();
+		for(StudentBean s : surveys) {
+			System.out.println(s.getId());
+			s1.add(s.getId());
+		}
+		return s1;
 	}
+	
 	
 	public List<StudentBean> readStudentDB() throws Exception {
 		List<StudentBean>s = new ArrayList<StudentBean>() ;
 		// create a query to select all the student ids
-		Query qry = entityManager.createQuery("select * from StudentDAO");
+		/*Query qry = entityManager.createQuery("select * from StudentDAO");
 		@SuppressWarnings("unchecked")
 		List<Object> studList = qry.getResultList();
 		for (Object std: studList){
 			s.add(((StudentDAO) std).convert());
-		}
+		}*/
 		return s;
 	}
-
 
 	/**
 	 * Method to query the database based on the id passed to it and return the
@@ -123,16 +133,18 @@ public class StudentDAOImpl {
 	 * @return student object
 	 * @throws Exception if the student cannot be retrieved
 	 */
-	public StudentBean readStudent(int id) throws Exception {
+	public StudentBean readStudent(String id) throws Exception {
 		// create a query to select the student based on the id
-		Query qry = entityManager.createQuery("select std from StudentDAO std where id=" + id);
+		/*Query qry = entityManager.createQuery("select std from StudentDAO std where id=" + id);
 		// get the single result of the query execution and store in object
 		Object stuDao = qry.getSingleResult();
 		// cast the object to StudentDAO and access the convert function to create the
 		// equivalent studentBean object
 		StudentBean student = ((StudentDAO) stuDao).convert();
-		// return the student bean
-		return student;
+		// return the student bean*/
+		for(StudentBean s : cache.get("qe")) if(s.getId().equals(id)) return s;
+		return null; 
+		
 	}
 
 	public int deleteStudent(int id) throws Exception {
